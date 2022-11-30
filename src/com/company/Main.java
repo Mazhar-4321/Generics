611489eedc1f8e8e05f39a0c94b679b2fdbf12b8
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-30 07:47:18 +0530

    ADDED: Code to find Maximum Of 3 Floats Available At Second Position

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Find Maximum Problem Using Generics");
         Main main = new Main();
         System.out.println(  main.maxOf3Integers(7,9,11));
-        System.out.println(main.maxOf3Floats(2.1f,1.1f,0.7f));
+        System.out.println(main.maxOf3Floats(2.1f,11.1f,0.7f));
     }
     public <T extends Integer> Integer maxOf3Integers(T num1,T num2, T num3){
          Integer ans = num1.compareTo(num2)>=1?num1:num2;
