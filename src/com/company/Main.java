73878bd81d0bbc6405af224aaf5934af866c3263
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-30 07:41:18 +0530

    ADDED: Code to find Maximum Of 3 Integers Available At First Position

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem Using Generics");
+        Main main = new Main();
+        System.out.println(  main.maxOf3Integers(7,5,6));
+    }
+    public <T extends Integer> Integer maxOf3Integers(T num1,T num2, T num3){
+         Integer ans = num1.compareTo(num2)>=1?num1:num2;
+         ans=ans.compareTo(num3)>=1?ans:num3;
+         return ans;
     }
 }
