49c3abf974e7d4e519aad50a602ffc430aaa0163
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-30 07:41:50 +0530

    ADDED: Code to find Maximum Of 3 Integers Available At Second Position

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem Using Generics");
         Main main = new Main();
-        System.out.println(  main.maxOf3Integers(7,5,6));
+        System.out.println(  main.maxOf3Integers(7,9,6));
     }
     public <T extends Integer> Integer maxOf3Integers(T num1,T num2, T num3){
          Integer ans = num1.compareTo(num2)>=1?num1:num2;
